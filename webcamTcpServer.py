16d33e7d5d521bf83bfa4cde5b04023ce1c5c155
Shaurya Khetarpal <shauryakhetarpal@Shauryas-MacBook-Air.local>
2025-09-20 15:49:43 +0530

    improved latency

--- webcamTcpServer.py
+++ webcamTcpServer.py
@@ -4,7 +4,7 @@ import struct
 import threading
 import time
 
-PI_IP   = "10.151.32.192"  # Pi IP
+PI_IP   = "10.17.37.192"  # change according to location
 PI_PORT = 6000
 MAC_PORT = 5005
 
@@ -30,13 +30,13 @@ threading.Thread(target=listen_predictions, daemon=True).start()
 # ROI = (150, 50, 300, 300)  # must match Pi side
 
 
-FRAME_SKIP = 3             # send 1 in 3 frames
+FRAME_SKIP = 5  #this number was found optimal for latency
 
 cap = cv2.VideoCapture(0)
 frame_count = 0
 frame_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frame_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-roi_size = int(0.6 * frame_w)   # square box size
+roi_size = int(0.28 * frame_w)   # enough for hand
 x = (frame_w - roi_size) // 2   # left aligned to center
 y = (frame_h - roi_size) // 2   # top aligned to center
 ROI = (x, y, roi_size, roi_size)
@@ -53,7 +53,7 @@ while True:
     # Frame skipping
     if frame_count % FRAME_SKIP == 0:
         roi = frame[y:y+h, x:x+w]
-        _, encoded = cv2.imencode(".jpg", roi, [cv2.IMWRITE_JPEG_QUALITY, 70])
+        _, encoded = cv2.imencode(".jpg", roi, [cv2.IMWRITE_JPEG_QUALITY, 50]) #enough quality for our model
         data = encoded.tobytes()
 
         # Send with length prefix
